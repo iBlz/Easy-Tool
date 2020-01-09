85ff42d607f96fb87bb97f952dfd8db4a5aeb693
Toxic-Omega <58302540+Toxic-Omega@users.noreply.github.com>
2020-01-09 14:19:21 +0100

    Add files via upload

--- Easy-Tool.py
+++ Easy-Tool.py
@@ -2,7 +2,122 @@ import os
 import time
 
 def menu():
+
     os.system("clear")
+    time.sleep(1)
+    print("""
+
+  _                        _  _               
+ | |                      | |(_)              
+ | |      ___    __ _   __| | _  _ __    __ _ 
+ | |     / _ \  / _` | / _` || || '_ \  / _` |
+ | |____| (_) || (_| || (_| || || | | || (_| |
+ |______|\___/  \__,_| \__,_||_||_| |_| \__, |
+                                         __/ |
+                                        |___/ 
+
+""")
+    time.sleep(1)
+    os.system("clear")
+    print("""
+
+  _                        _  _                   
+ | |                      | |(_)                  
+ | |      ___    __ _   __| | _  _ __    __ _     
+ | |     / _ \  / _` | / _` || || '_ \  / _` |    
+ | |____| (_) || (_| || (_| || || | | || (_| |  _ 
+ |______|\___/  \__,_| \__,_||_||_| |_| \__, | (_)
+                                         __/ |    
+                                        |___/     
+
+""")
+    time.sleep(1)
+    os.system("clear")
+    print("""
+
+  _                        _  _                      
+ | |                      | |(_)                     
+ | |      ___    __ _   __| | _  _ __    __ _        
+ | |     / _ \  / _` | / _` || || '_ \  / _` |       
+ | |____| (_) || (_| || (_| || || | | || (_| |  _  _ 
+ |______|\___/  \__,_| \__,_||_||_| |_| \__, | (_)(_)
+                                         __/ |       
+                                        |___/        
+
+""")
+    time.sleep(1)
+    os.system("clear")
+    print("""
+
+  _                        _  _                         
+ | |                      | |(_)                        
+ | |      ___    __ _   __| | _  _ __    __ _           
+ | |     / _ \  / _` | / _` || || '_ \  / _` |          
+ | |____| (_) || (_| || (_| || || | | || (_| |  _  _  _ 
+ |______|\___/  \__,_| \__,_||_||_| |_| \__, | (_)(_)(_)
+                                         __/ |          
+                                        |___/                
+
+""")
+    os.system("clear")
+    time.sleep(1)
+    print("""
+
+  _                        _  _               
+ | |                      | |(_)              
+ | |      ___    __ _   __| | _  _ __    __ _ 
+ | |     / _ \  / _` | / _` || || '_ \  / _` |
+ | |____| (_) || (_| || (_| || || | | || (_| |
+ |______|\___/  \__,_| \__,_||_||_| |_| \__, |
+                                         __/ |
+                                        |___/ 
+
+""")
+    time.sleep(1)
+    os.system("clear")
+    print("""
+
+  _                        _  _                   
+ | |                      | |(_)                  
+ | |      ___    __ _   __| | _  _ __    __ _     
+ | |     / _ \  / _` | / _` || || '_ \  / _` |    
+ | |____| (_) || (_| || (_| || || | | || (_| |  _ 
+ |______|\___/  \__,_| \__,_||_||_| |_| \__, | (_)
+                                         __/ |    
+                                        |___/     
+
+""")
+    time.sleep(1)
+    os.system("clear")
+    print("""
+
+  _                        _  _                      
+ | |                      | |(_)                     
+ | |      ___    __ _   __| | _  _ __    __ _        
+ | |     / _ \  / _` | / _` || || '_ \  / _` |       
+ | |____| (_) || (_| || (_| || || | | || (_| |  _  _ 
+ |______|\___/  \__,_| \__,_||_||_| |_| \__, | (_)(_)
+                                         __/ |       
+                                        |___/        
+
+""")
+    time.sleep(1)
+    os.system("clear")
+    print("""
+
+  _                        _  _                         
+ | |                      | |(_)                        
+ | |      ___    __ _   __| | _  _ __    __ _           
+ | |     / _ \  / _` | / _` || || '_ \  / _` |          
+ | |____| (_) || (_| || (_| || || | | || (_| |  _  _  _ 
+ |______|\___/  \__,_| \__,_||_||_| |_| \__, | (_)(_)(_)
+                                         __/ |          
+                                        |___/                
+
+""")
+    time.sleep(2)
+    os.system("clear")
+    
     print("""
     
   ______                     _______             _ 
@@ -14,12 +129,12 @@ def menu():
                       __/ |                        
                      |___/                         
 
-[ 1 ] Nmap
+[ 1 ] Kali-Linux
 [ 2 ] 
 [ 3 ] 
 [ 4 ] 
-[ 5 ] 
 [ 6 ] 
+[ 5 ] 
 [ 7 ] 
 [ 8 ] 
 [ 9 ] 
@@ -32,302 +147,25 @@ while loop:
     if what == "1":
         os.system("clear")
         print(" ")
-        print("Installing Nmap...")
+        print("Installing Kali-Linux...")
         print(" ")
         time.sleep(4)
-        os.system("apt update")
-        os.system("apt upgrade")
-        os.system("git clone https://github.com/nmap/nmap")
-        os.system("cd nmap")
+        os.system("cd /data/data/com.termux/files/home")
+        os.system("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh")
+        os.system("cd /data/data/com.termux/files/home")
         os.system("clear")
         print(" ")
-        print("Nmap sucesfully installed...")
+        print("Kali-Linux sucesfully installed...")
         print(" ")
         rmenu = input("Back to Menu? (y/n): ")
         if rmenu == "y":
-            loading()
-        else:
-            break
-    elif what == "2":
-        os.system("cd /data/data/com.termux/files/home")
-        os.system("pkg update -y")
-        os.system("pkg install -y hydra")
-        os.system("cd /data/data/com.termux/files/home")
-        print("====================================")
-        print("[+] hydra installed successfully :)")
-        print("[+] Type 'hydra' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
             menu()
         else:
             break
-    elif what == "3":
-        os.system("cd /data/data/com.termux/files/home")
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("pkg install python2")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/sqlmapproject/sqlmap.git")
-        os.system("cd /data/data/com.termux/files/home")
-        print("====================================")
-        print("[+] SQLMap installed successfully :)")
-        print("[+] Go to sqlmap folder and type 'python2 sqlmap.py' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "4":
-        os.system("pkg install -y wget")
-        os.system("cd /data/data/com.termux/files/home && wget https://Auxilus.github.io/metasploit.sh")
-        os.system("cd /data/data/com.termux/files/home && bash metasploit.sh")
-        os.system("cd /data/data/com.termux/files/home && cd metasploit-framework")
-        os.system("cd /data/data/com.termux/files/home && gem install bundle --pre")
-        os.system("cd /data/data/com.termux/files/home && bundle config build.nokogiri --use-system-libraries")
-        os.system("cd /data/data/com.termux/files/home && bundle install")
-        os.system("cd /data/data/com.termux/files/home")
-        print("====================================")
-        print("[+] Metasploit installed successfully :)")
-        print("[+] Type 'msfconsole' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "5":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/themastersunil/ngrok.git")
-        os.system("cd /data/data/com.termux/files/home")
-        print("====================================")
-        print("[+] ngrok installed successfully :)")
-        print("[+] Go to ngrok folder and type './ngrok http 80' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "6":
-        os.system("pkg update -y")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Hax4us/Nethunter-In-Termux.git")
-        os.system("cd /data/data/com.termux/files/home && cd Nethunter-In-Termux && chmod +x kalinethunter")
-        os.system("cd /data/data/com.termux/files/home")
-        print("====================================")
-        print("[+] Nethunter installed successfully :)")
-        print("[+] Go to Nethunter-In-Termux folder and type './kalinethunter' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "7":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("pkg install -y python2")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/ihebski/angryFuzzer.git")
-        os.system("cd /data/data/com.termux/files/home && cd angryFuzzer")
-        os.system("cd /data/data/com.termux/files/home && pip2 install -r requirements.txt")
-        os.system("cd /data/data/com.termux/files/home && pip2 install requests")
-        os.system("cd /data/data/com.termux/files/home")
-        print("====================================")
-        print("[+] angryFuzzer installed successfully :)")
-        print("[+] Go to angryFuzzer folder and type 'python2 angryFuzzer.py' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "8":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("pkg install -y php")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Tuhinshubhra/RED_HAWK")
-        os.system("cd /data/data/com.termux/files/home")
-        print("====================================")
-        print("[+] RED_HAWK installed successfully :)")
-        print("[+] Go to RED_HAWK folder and type 'php rhawk.php' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "9":
-        os.system("pkg update -y")
-        os.system("pkg install -y python2")
-        os.system("pkg install -y git")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/evait-security/weeman.git")
-        os.system("cd /data/data/com.termux/files/home && cd weeman")
-        os.system("cd /data/data/com.termux/files/home && chmod +x weeman.py")
-        os.system("cd /data/data/com.termux/files/home")
-        print("====================================")
-        print("[+] weeman installed successfully :)")
-        print("[+] Go to weeman folder and type 'python2 weeman.py' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "10":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("pkg install -y python")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/maldevel/IPGeoLocation.git")
-        os.system("cd /data/data/com.termux/files/home && cd IPGeoLocation")
-        os.system("cd /data/data/com.termux/files/home && pip install -r requirements.txt")
-        os.system("cd /data/data/com.termux/files/home")
-        print("====================================")
-        print("[+] IPGeoLocation installed successfully :)")
-        print("[+] Go to IPGeoLocation folder and type 'python ipgeolocation.py' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "11":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("pkg install -y python")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Mebus/cupp.git")
-        print("====================================")
-        print("[+] Cupp installed successfully :)")
-        print("[+] Go to cupp folder and type 'python cupp3.py' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "12":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("pkg install -y python")
-        os.system("pkg install -y nano")
-        os.system("pip install requests")
-        os.system("pip install beautifulsoup4")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/avramit/instahack.git")
-        print("====================================")
-        print("[+] Instahack installed successfully :)")
-        print("[+] Go to instahack folder and type 'python hackinsta.py' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "13":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("pkg install -y python")
-        os.system("pip install mechanicalsoup")
-        os.system("pkg install -y nano")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/abdallahelsokary/TwitterSniper.git")
-        print("====================================")
-        print("[+] TwitterSniper installed successfully :)")
-        print("[+] Go to TwitterSniper folder and type 'python TwitterSniper.py' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "14":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Neo-Oli/termux-ubuntu.git")
-        os.system("cd /data/data/com.termux/files/home && cd termux-ubuntu && bash ubuntu.sh")
-        print("====================================")
-        print("[+] Ubuntu installed successfully :)")
-        print("[+] Go to termux-ubuntu folder and type './start.sh' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "15":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("pkg install -y wget")
-        os.system("apt update && apt install wget -y && /data/data/com.termux/files/usr/bin/wget https://raw.githubusercontent.com/nmilosev/termux-fedora/master/termux-fedora.sh")
-        print("====================================")
-        print("[+] Fedora installed successfully :)")
-        print("[+] Type 'sh termux-fedora.sh f26_arm64' or 'sh termux-fedora.sh f26_arm' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "16":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("pkg install -y python2")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/blackvkng/viSQL.git")
-        print("====================================")
-        print("[+] viSQL installed successfully :)")
-        print("[+] Go to viSQL folder and type 'python2 viSQL.py --help' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "17":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("pkg install -y python2")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/UltimateHackers/Hash-Buster.git")
-        print("====================================")
-        print("[+] Hash-Buster installed successfully :)")
-        print("[+] Go to Hash-Buster folder and type 'python2 hash.py' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-    elif what == "18":
-        os.system("pkg update -y")
-        os.system("pkg install -y git")
-        os.system("pkg install -y python2")
-        os.system("cd /data/data/com.termux/files/home && git clone https://github.com/shawarkhanethicalhacker/D-TECT.git")
-        print("====================================")
-        print("[+] Hash-Buster installed successfully :)")
-        print("[+] Go to Hash-Buster folder and type 'python2 hash.py' to start.")
-        print("====================================")
-        rmenu = input("[?] Back to Menu? (y/n): ")
-        if rmenu == "y":
-            menu()
-        else:
-            break
-            os.system("pkg update -y")
-            os.system("pkg install -y git")
-            os.system("pkg install -y python2")
-            os.system("cd /data/data/com.termux/files/home && git clone https://github.com/reverse-shell/routersploit.git")
-            os.system("cd /data/data/com.termux/files/home && cd routersploit")
-            os.system("pip2 install -r requirements.txt")
-            os.system("pip2 install -r requirements-dev.txt")
-            os.system("pip2 install -r requests")
-            print("====================================")
-            print("[+] routersploit installed successfully :)")
-            print("[+] Go to routersploit folder and type 'python2 rsf.py' to start.")
-            print("====================================")
-            rmenu = input("[?] Back to Menu? (y/n): ")
-            if rmenu == "y":
-                menu()
-            else:
-                break
     elif what == "x":
         os.system("clear")
         print(" ")
         print("Exiting...")
         print(" ")
+        exit
         break
